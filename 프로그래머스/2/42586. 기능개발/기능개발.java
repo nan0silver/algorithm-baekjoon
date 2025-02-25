8b77c5231d565aef8b33b5fd10b4aee61bb5451e
NaHyun Eun <silvernh1220@gmail.com>
2025-02-25 12:38:50 +0900

    [level 2] Title: 기능개발, Time: 3.32 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -3,31 +3,14 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] answer = {};
-        Queue<Integer> queue = new LinkedList<>();
-        ArrayList<Integer> al_answer = new ArrayList<>();
-
+        int[] dayOfEnd = new int[100];
+        int day = -1;
         for (int i = 0; i < progresses.length; i++) {
-            int p = progresses[i];
-            int s = speeds[i];
-            int day = (int) Math.ceil((100.0-p)/s);
-            queue.offer(day);
-        }
-
-        int max = queue.poll();
-        int num=1;
-        while(!queue.isEmpty()){
-            if (queue.peek() <= max){
-                queue.poll();
-                num++;
-            } else {
-                al_answer.add(num);
-                max = queue.poll();
-                num = 1;
+            while ((progresses[i] + day*speeds[i]) < 100) {
+                day++;
             }
+            dayOfEnd[day]++;
         }
-        al_answer.add(num);
-        answer = al_answer.stream().mapToInt(i -> i).toArray();
-        return answer;
+        return Arrays.stream(dayOfEnd).filter(i -> i!=0).toArray();
     }
 }
