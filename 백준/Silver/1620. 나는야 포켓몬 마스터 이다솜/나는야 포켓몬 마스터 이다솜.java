4c57e7819c194624c25a7189afa20230b4086fbf
NaHyun Eun <silvernh1220@gmail.com>
2025-02-26 17:48:29 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 464 ms, Memory: 52192 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
@@ -13,18 +14,18 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        HashMap<String, String> hashMap = new HashMap<>(N);
+        ArrayList<String> arrayList = new ArrayList<>();
         HashMap<String, String> hashMap2 = new HashMap<>(N);
         for (int i = 1; i <= N; i++) {
             String tmp = br.readLine();
-            hashMap.put(Integer.toString(i), tmp);
+            arrayList.add(tmp);
             hashMap2.put(tmp, Integer.toString(i));
         }
 
         for (int i = 0; i < M; i++) {
             String tmp = br.readLine();
-            if (hashMap.containsKey(tmp))sb.append(hashMap.get(tmp)).append("\n");
-            else sb.append(hashMap2.get(tmp)).append("\n");
+            if (hashMap2.containsKey(tmp))sb.append(hashMap2.get(tmp)).append("\n");
+            else sb.append(arrayList.get(Integer.parseInt(tmp)-1)).append("\n");
         }
         System.out.println(sb);
 
