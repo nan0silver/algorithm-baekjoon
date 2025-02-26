157f5fe6fe4b68f5d6e9716572221def8788f1a2
NaHyun Eun <silvernh1220@gmail.com>
2025-02-26 16:20:45 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 948 ms, Memory: 170680 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -7,27 +7,22 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
+        HashMap<Integer, Integer> hashMap = new HashMap<>(N);
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < N; i++) {
             int tmp = Integer.parseInt(st.nextToken());
-            if(hashMap.containsKey(tmp)){
-                int value = hashMap.get(tmp);
-                hashMap.replace(tmp, value+1);
-            } else {
-                hashMap.put(tmp, 1);
-            }
+            hashMap.put(tmp, hashMap.getOrDefault(tmp, 0)+1);
         }
 
         int M = Integer.parseInt(br.readLine());
         StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < M; i++) {
             int tmp2 = Integer.parseInt(st2.nextToken());
-            if (hashMap.containsKey(tmp2)) sb.append(hashMap.get(tmp2)).append(" ");
-            else sb.append("0 ");
+            sb.append(hashMap.getOrDefault(tmp2, 0)).append(" ");
         }
 
         System.out.println(sb);
