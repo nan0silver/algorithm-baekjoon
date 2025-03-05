3f744cd63934e35a6066b2a7e0e33fc01d91f040
NaHyun Eun <silvernh1220@gmail.com>
2025-03-05 17:37:52 +0900

    [Silver IV] Title: 수 찾기, Time: 528 ms, Memory: 50796 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -2,25 +2,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashSet;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        String[] nArray = br.readLine().split(" ");
         StringBuilder sb = new StringBuilder();
 
         HashSet<Integer> set = new HashSet<>(N);
-        for (String arr : nArray) {
-            set.add(Integer.parseInt(arr));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        while (st.hasMoreTokens()) {
+            set.add(Integer.parseInt(st.nextToken()));
         }
 
         int M = Integer.parseInt(br.readLine());
-        String[] mArray = br.readLine().split(" ");
+        st = new StringTokenizer(br.readLine(), " ");
 
-        for (String m : mArray) {
-            int mTmp = Integer.parseInt(m);
-            sb.append(set.contains(mTmp) ? "1" : "0").append("\n");
+        while (st.hasMoreTokens()) {
+            int query = Integer.parseInt(st.nextToken());
+            sb.append(set.contains(query)? "1\n" : "0\n");
         }
         System.out.println(sb);
 
