aed909855e77f5529d66ccde302b1d18a01112e8
NaHyun Eun <silvernh1220@gmail.com>
2025-10-28 11:27:59 +0900

    [level 1] Title: 모의고사, Time: 0.93 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,38 +1,28 @@
-import java.io.*;
 import java.util.*;
-
 class Solution {
-    public int[] solution(int[] answers) {
-        int[][] patterns = {
-                {1, 2, 3, 4, 5},
-                {2, 1, 2, 3, 2, 4, 2, 5},
-                {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
-        };
-
-        int[] score = new int[3];
-
-        for (int i = 0; i < answers.length; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (answers[i] == patterns[j][i % patterns[j].length]){
-                    score[j]++;
-                }
-            }
+    public List<Integer> solution(int[] answers) {
+        List<Integer> answer = new ArrayList<>();
+        
+        int[] p1 = {1, 2, 3, 4, 5}; //5
+        int[] p2 = {2, 1, 2, 3, 2, 4, 2, 5}; //8
+        int[] p3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}; //10
+        
+        int numsOfProblems = answers.length;
+        int a1=0, a2=0, a3=0;
+        
+        for (int i = 0; i < numsOfProblems; i++) {
+            if (answers[i] == p1[i%5]) a1++;
+            if (answers[i] == p2[i%8]) a2++;
+            if (answers[i] == p3[i%10]) a3++;
         }
-
-        int maxScore = Math.max(score[0], Math.max(score[1], score[2]));
-
-        int bestScoreStudent = 0;
-        for (int s : score){
-            if (s == maxScore) bestScoreStudent++;
-        }
-
-        int[] answer = new int[bestScoreStudent];
-        int index = 0;
-        for (int i = 0; i < 3; i++){
-            if (score[i] == maxScore) {
-                answer[index++] = i+1;
-            }
-        }
+        
+        int max = Math.max(a1, Math.max(a2, a3));
+        
+        if (a1 == max) answer.add(1);
+        if (a2 == max) answer.add(2);
+        if (a3 == max) answer.add(3);
+        
+        
         return answer;
     }
 }
