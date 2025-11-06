a424e0228d0a674febc2ab8b84b9ec167e8e0213
NaHyun Eun <silvernh1220@gmail.com>
2025-11-06 15:23:07 +0900

    [level 3] Title: 네트워크, Time: 0.37 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    static boolean[] visited;
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        visited = new boolean[n];
+        
+        for (int k = 0; k < n; k++ ){
+            if (!visited[k]) {
+                bfs(n, k, computers);
+                answer++;
+            }
+        }
+        return answer;
+    }
+    public void bfs(int n, int start, int[][] computers) {
+        
+        Deque<Integer> queue = new ArrayDeque<>();
+        queue.offer(start);
+        visited[start] = true;
+        
+        while(!queue.isEmpty()) {
+            int tmp = queue.poll();
+            for (int i = 0; i < n; i++) {
+                if (computers[tmp][i] == 1 && i != tmp && !visited[i]) {
+                    queue.offer(i);
+                    visited[i] = true;
+                }
+            }
+        }
+        
+    }
+}
