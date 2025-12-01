2d9b14f7ce1fd674e12d2783a6475000bd3ae0ca
NaHyun Eun <silvernh1220@gmail.com>
2025-12-01 14:13:12 +0900

    [Silver II] Title: A → B, Time: 100 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int A = Integer.parseInt(input[0]);
+        int B = Integer.parseInt(input[1]);
+
+        int count = 1;
+
+        while (B > A) {
+            if (B % 10 == 1) {
+                B /= 10;
+                count++;
+            } else if (B % 2 == 0) {
+                B /= 2;
+                count++;
+            } else {
+                count = -1;
+                break;
+            }
+
+        }
+        if (B < A) count = -1;
+        System.out.println(count);
+    }
+
+
+
+
+}
