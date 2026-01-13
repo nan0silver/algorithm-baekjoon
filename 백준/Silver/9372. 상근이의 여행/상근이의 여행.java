aa8b4c3ef530a11474d5269e995644a0c8eb07f0
NaHyun Eun <silvernh1220@gmail.com>
2026-01-13 10:22:02 +0900

    [Silver IV] Title: 상근이의 여행, Time: 356 ms, Memory: 58324 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int t = 0; t < T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            for (int m = 0; m < M; m++) {
+                String[] input = br.readLine().split(" ");
+            }
+            sb.append(N-1).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+}
