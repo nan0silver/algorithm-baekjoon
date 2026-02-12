e9999764e792d5eda2658fc2efd5f7770a0679da
NaHyun Eun <silvernh1220@gmail.com>
2026-02-12 14:33:10 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 87.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        
+        for (int n : numbers) answer += n;
+        
+        return 45-answer;
+    }
+}
