cc178593cdd66ae67beaf2d7271d9f54b3bc356e
NaHyun Eun <silvernh1220@gmail.com>
2026-02-26 13:40:37 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 54.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int ans = 0;
+
+        while (n > 0) {
+            if ((n & 1) == 1) ans++;
+            n >>= 1;
+        }
+        return ans;
+    }
+}
